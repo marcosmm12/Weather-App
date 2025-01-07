ec2863bf99d6fb79a0b3c33d21da54a911660bc4
marcosmm12 <marcosmmartinez12@gmail.com>
2025-01-07 11:30:10 +0100

    codigo sin partes innecesarias

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { get_temperature, get_precipitation, get_wind } from "./api";
 
 //Coordinates of some cities
@@ -473,37 +473,4 @@ function App() {
 
 };
 
-export default App
-
-
-{/*Default code of the app*/}
-/*
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-  */
+export default App;
